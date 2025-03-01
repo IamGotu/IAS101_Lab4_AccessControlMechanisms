d1e30f64ed00e15a12d6298461dacf1c9ec43215
Mark John <markjohnjopia1@gmail.com>
2025-03-02 04:38:17 +0800

    dashboard html

---
+++ src/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <script type="module" src="../js/dashboard.js"></script>
+</head>
+<body>
+    <h1>Welcome to the Dashboard</h1>
+    <div>First Name: <span id="loggedUserFirstName"></span></div>
+    <div>Middle Name: <span id="loggedUserMiddleName"></span></div>
+    <div>Last Name: <span id="loggedUserLastName"></span></div>
+    <div>Email: <span id="loggedUserEmail"></span></div>
+    <h1><a href="login_sign_up.php" id="logout">Logout</a></h1>
+</body>
+</html>
