b8a4c7bf68ac1dc0f8d960a184aa1e8a90e8bd50
Mark John <160160952+IamGotu@users.noreply.github.com>
2025-06-10 01:01:32 +0800

    sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+    <div class="user-info">
+        <h2 id="loggedUserFullName"></h2>
+        <span id="loggedUserEmail"></span>
+        <span id="loggedUserRole"></span>
+    </div>
+    <ul>
+        <li><a href="../src/dashboard.php">Dashboard</a></li>
+        <li><a href="../src/manage_users.php">Manage Users</a></li>
+        <li><a href="#" id="logout">Logout</a></li>
+    </ul>
+</div>
